38a2aa1e3fb259ed87201caf3256fba6cca3b963
unknown <lleyzagz@gmail.com>
2026-09-04 16:11:46 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/CircleCalculator.kt
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/CircleCalculator.kt
@@ -68,3 +68,16 @@ fun checkSize(area: Double) {
         println("This is a Small Circle")
     }
 }
+
+package oop_00000153666_KeyzaNuryanaAlFaridzi.week01
+
+fun main(args: Array<String>) {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
+
+    println(checkSize(area))
+}
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
