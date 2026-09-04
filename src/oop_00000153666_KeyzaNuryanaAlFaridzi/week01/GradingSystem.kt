02ca0f17c46e3c246d1b547903609db8b76fbac3
unknown <lleyzagz@gmail.com>
2026-09-04 13:19:46 +0700

    week01: refactor to use val and string templates

---
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/GradingSystem.kt
@@ -0,0 +1,16 @@
+package oop_00000153666_KeyzaNuryanaAlFaridzi.week01
+
+fun main() {
+   var name: string = "Keyza"
+    var score : int = 80
+
+    // concatenation gaya lama
+    println ("name:" + name + ", nilai :" + score )
+}
+
+fun main(){
+    val name = "keyza"
+    val score = 80
+
+    println("nama: $name, nilai: $score")
+}
