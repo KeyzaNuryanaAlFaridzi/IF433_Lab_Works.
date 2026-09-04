e76f501e5fefd6f091d72ac898dd8b9930556925
unknown <lleyzagz@gmail.com>
2026-09-04 15:51:50 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/CircleCalculator.kt
@@ -0,0 +1,22 @@
+package oop_00000153666_KeyzaNuryanaAlFaridzi.week01
+
+fun main (args: Array<String>){
+// Variable definition
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+
+// Calculation
+    var area: Double * = radius pi * radius
+}
+
+// Output Concatenation
+println("Radius: + radius + Area: 11 + area)
+// Logic check
+checkSize (area)
+}
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+    }
+    println("This is a Small Circle")
