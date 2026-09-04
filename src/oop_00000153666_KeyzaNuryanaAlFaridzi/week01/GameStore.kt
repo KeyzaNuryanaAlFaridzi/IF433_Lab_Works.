b4232ea922756f129cf8a86681a6d5494e5abb3c
unknown <lleyzagz@gmail.com>
2026-09-04 19:25:09 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/GameStore.kt
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/GameStore.kt
@@ -3,4 +3,11 @@ package oop_00000153666_KeyzaNuryanaAlFaridzi.week01
 fun main(){
     val gametitle: string = "streakw"
     val price: int = "600000"
+
+    val discount = calculateDiscount (price)
+    val finalprice = price -(price * discount / 100)
 }
+
+fun  calculateDiscount(price :int): int = if (price > 500000) 20 else 10
+
+
