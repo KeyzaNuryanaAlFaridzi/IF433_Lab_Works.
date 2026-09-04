a2688bd86ab9f84973a8a85a7798986481e691af
unknown <lleyzagz@gmail.com>
2026-09-04 16:16:05 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/CircleCalculator.kt
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week01/CircleCalculator.kt
@@ -81,3 +81,14 @@ fun main(args: Array<String>) {
     println(checkSize(area))
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
+
+fun main() {
+    val radius = 7.0
+    val pi = 3.14
+    val area = pi * radius * radius
+
+    println("Radius: $radius, Area: $area")
+
+    println(checkSize(area))
+}
+fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
