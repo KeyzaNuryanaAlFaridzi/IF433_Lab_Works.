eaa1899b514761e247e59673110a61013e5b564d
unknown <lleyzgz@gmail.com>
2026-09-05 10:31:18 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi_week02/Student.main.kts
@@ -0,0 +1,11 @@
+package oop_00000153666_KeyzaNuryanaAlFaridzi_week02
+
+
+class Student (new
+               val name: String,
+               val nim: String,
+               var major: String
+{
+// Body class kosong dulu
+}
+
