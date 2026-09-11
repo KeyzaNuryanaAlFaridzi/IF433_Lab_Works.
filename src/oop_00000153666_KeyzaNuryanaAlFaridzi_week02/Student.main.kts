d733dc4fc8281aa6028531e86c5b57bfe91134ae
unknown <lleyzagz@gmail.com>
2026-09-11 17:48:49 +0700

    week02: add secondary constructor

--- src/oop_00000153666_KeyzaNuryanaAlFaridzi_week02/Student.main.kts
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi_week02/Student.main.kts
@@ -1,20 +1,60 @@
-package oop_00000153666_KeyzaNuryanaAlFaridzi_week02
+package oop_001_johnthor.week02
 
+import java.util.Scanner
 
-class Student (
-               val name: String,
-               val nim: String,
-               var major: string
-)
-// Body class kosong dulu
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
 
-class Student (val name: String, val nim: String, var major: String) { new*
-    init { new *
-// Validasi Sederhana: Cek panjang NIM
-            if (nim.length != 5) {
-                println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-                println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-            } else {
-                println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-            }
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner!)
+
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
+
+
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
+) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+
+
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+}
+
