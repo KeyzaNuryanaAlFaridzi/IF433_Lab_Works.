885e5423be578a84c76cdd06cee9f0b8ad6be1e6
unknown <lleyzagz@gmail.com>
2026-09-26 10:17:38 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000153666_KeyzaNuryanaAlFaridzi/week05/pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000153666_KeyzaNuryanaAlFaridzi.week05
+
+abstract class pegawai(val nama: String) {
+    // Abstract function tidak memiliki body Eycypg kurawat)
+    abstract fun bekerja()
+}
